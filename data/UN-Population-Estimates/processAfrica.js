cedf3ab4ab208cb2f2c88314a05a57e901e39f2b
curran <curran.kelleher@gmail.com>
2017-09-26 00:14:42 +0530

    Add Africa future estimates

--- data/UN-Population-Estimates/processAfrica.js
+++ data/UN-Population-Estimates/processAfrica.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const d3 = require('d3');
-const csvIn = fs.readFileSync('wpp2017.csv', 'utf8');
-const dataIn = d3.csvParse(csvIn);
+const csv1In = fs.readFileSync('wpp2017.csv', 'utf8');
+const csv2In = fs.readFileSync('wpp2017-medium-variant.csv', 'utf8');
+const data1In = d3.csvParse(csv1In);
+const data2In = d3.csvParse(csv2In);
+const dataIn = data1In.map((d, i) => Object.assign(d, data2In[i]));
 const name = d => d['Region, subregion, country or area *'];
-const years = d3.range(1950, 2015 + 1);
+const years = d3.range(1950,2100 + 1);
 const africa = dataIn.filter(d => name(d) === 'AFRICA')[0];
 const dataOut = years.map(year => ({
   year,
